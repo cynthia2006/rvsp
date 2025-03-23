50f9d2efdf600b949a389c7ba249dd8d66496ada
cynthia2006 <121848440+cynthia2006@users.noreply.github.com>
2025-03-23 17:30:45 +0530

    Minor performance fixes + Return resizability

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::f32::consts::{PI, SQRT_2};
-use std::ffi::CString;
 use std::ptr;
 
 use circular_buffer::CircularBuffer;
 
 use sdl3::audio::{AudioFormat, AudioRecordingCallback, AudioSpec, AudioStream};
-use sdl3::event::Event;
+use sdl3::event::{Event, WindowEvent};
 use sdl3::keyboard::Keycode;
 use sdl3::video::GLProfile;
 
@@ -14,14 +13,13 @@ use realfft::RealFftPlanner;
 
 use gl;
 
-use lazy_static::lazy_static;
-
-mod utils;
+mod shaders;
 
 const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 const MARGIN_VW: f32 = 0.01;
 const LINE_WIDTH: f32 = 1.5;
+const MSAA: u8 = 8; // 8x MSAA.
 
 const SAMPLERATE: i32 = 48000;
 const FFT_SIZE: usize = 2048;
@@ -40,14 +38,16 @@ fn make_window_title(gain: f32) -> String {
     )
 }
 
-fn hann_window(n: usize) -> impl Iterator<Item = f32> {
+fn hann_window(win: &mut [f32]) {
     const A0: f32 = 0.5;
 
-    (0..n).map(move |i| {
-        let y: f32 = (2.0 * PI * i as f32 / n as f32).cos();
+    let n = win.len();
 
-        A0 - (1.0 - A0) * y
-    })
+    for (i, w) in win.iter_mut().enumerate() {
+        let x: f32 = (2.0 * PI * i as f32 / n as f32).cos();
+
+        *w = A0 - (1.0 - A0) * x;
+    }
 }
 
 #[inline(always)]
@@ -87,28 +87,6 @@ impl<const N1: usize, const N2: usize> AudioRecordingCallback<f32> for AudioReco
     }
 }
 
-lazy_static! {
-    static ref VERTEX_SHADER_SRC: CString = CString::new(
-        r"#version 330 core
-    layout (location = 0) in vec2 coord;
-
-    void main() {
-        gl_Position = vec4(coord.xy, 0.0f, 1.0f);
-    }
-    "
-    )
-    .unwrap();
-    static ref FRAGMENT_SHADER_SRC: CString = CString::new(
-        r"#version 330 core
-    out vec4 FragColor;
-
-    void main() {
-        FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }"
-    )
-    .unwrap();
-}
-
 fn main() {
     let sdl = sdl3::init().unwrap();
     let sdl_audio = sdl.audio().unwrap();
@@ -118,11 +96,13 @@ fn main() {
     let mut planner = RealFftPlanner::new();
     let fft = planner.plan_fft_forward(FFT_SIZE as usize);
     let mut fft_scratch = fft.make_scratch_vec();
-    let window_fn: Vec<f32> = hann_window(FFT_SIZE as usize).collect();
+    let mut window_fn = [0.0; FFT_SIZE];
     let mut windowed_signal = Box::new([0.0; FFT_SIZE]);
     let mut frequency_bins = Box::new([Complex32::default(); FFT_SIZE / 2 + 1]);
     let mut gain = GAIN;
     let mut gain_mult = db_rms_to_factor(gain);
+
+    hann_window(&mut window_fn);
 
     const BIN_WIDTH: f32 = FFT_SIZE as f32 / SAMPLERATE as f32;
     const LOW_BIN: usize = (BIN_WIDTH * MIN_FREQ as f32) as usize;
@@ -130,7 +110,7 @@ fn main() {
     const BANDWIDTH: usize = HIGH_BIN - LOW_BIN;
 
     let mut smoothed_fft = vec![0.0; BANDWIDTH];
-    let mut plot = [0f32; 2 * BANDWIDTH];
+    let mut plot = [0f32; BANDWIDTH + 1];
 
     const BUF_SIZE: usize = FFT_SIZE / 2;
     let mut stream = sdl_audio
@@ -148,12 +128,12 @@ fn main() {
     gl_attr.set_context_profile(GLProfile::Core);
     gl_attr.set_context_version(3, 3);
     gl_attr.set_multisample_buffers(1);
-    gl_attr.set_multisample_samples(2);
+    gl_attr.set_multisample_samples(MSAA);
 
     let mut window = sdl_video
         .window(&make_window_title(gain), WIDTH, HEIGHT)
         .position_centered()
-        // .resizable()
+        .resizable()
         .opengl()
         .build()
         .unwrap();
@@ -165,49 +145,72 @@ fn main() {
     gl::load_with(|name| sdl_video.gl_get_proc_address(name).unwrap() as *const _);
 
     let mut vao = 0;
-    let mut vbo = 0;
+    let mut vbo_x = 0;
+    let mut vbo_y = 0;
     let program = unsafe { gl::CreateProgram() };
 
     unsafe {
         let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
         let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
 
-        gl::ShaderSource(vertex_shader, 1, &VERTEX_SHADER_SRC.as_ptr(), ptr::null());
+        gl::ShaderSource(vertex_shader, 1, &shaders::VERTEX_SHADER_SRC.as_ptr(), ptr::null());
         gl::CompileShader(vertex_shader);
-        utils::ensure_shader_compilation(vertex_shader).expect("Vertex shader compilation failed");
 
-        gl::ShaderSource(
-            fragment_shader,
-            1,
-            &FRAGMENT_SHADER_SRC.as_ptr(),
-            ptr::null(),
-        );
+        gl::ShaderSource(fragment_shader, 1,&shaders::FRAGMENT_SHADER_SRC.as_ptr(),ptr::null());
         gl::CompileShader(fragment_shader);
-        utils::ensure_shader_compilation(fragment_shader)
-            .expect("Fragment shader compilation failed");
 
         gl::AttachShader(program, vertex_shader);
         gl::AttachShader(program, fragment_shader);
         gl::LinkProgram(program);
-        utils::ensure_shader_linking(program).expect("Shader linking failed");
 
         gl::DeleteShader(vertex_shader);
         gl::DeleteShader(fragment_shader);
+    }
 
+    /* Generate X coordinates once; reused by GPU infinite times. */
+    plot[0] = MARGIN_VW - 1.0;
+
+    const X_STEP: f32 = 2.0 * (1.0 - MARGIN_VW) / BANDWIDTH as f32;
+    let mut x = X_STEP + MARGIN_VW - 1.0;
+    
+    for x_coord in plot[1..].iter_mut() {
+        *x_coord = x;
+        x += X_STEP;
+    }
+
+    unsafe {
         gl::GenVertexArrays(1, &mut vao);
-        gl::GenBuffers(1, &mut vbo);
+        gl::GenBuffers(1, &mut vbo_x);
+        gl::GenBuffers(1, &mut vbo_y);
 
         gl::BindVertexArray(vao);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo_x);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (plot.len() * size_of::<f32>()) as isize,
+            plot.as_ptr() as *const _,
+            gl::STATIC_DRAW,
+        );
         gl::VertexAttribPointer(
             0,
-            2,
+            1,
             gl::FLOAT,
             gl::FALSE,
-            2 * size_of::<f32>() as i32,
+            size_of::<f32>() as i32,
             0 as *const _,
         );
         gl::EnableVertexAttribArray(0);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo_y);
+        gl::VertexAttribPointer(
+            1,
+            1,
+            gl::FLOAT,
+            gl::FALSE,
+            size_of::<f32>() as i32,
+            0 as *const _,
+        );
+        gl::EnableVertexAttribArray(1);
 
         gl::LineWidth(LINE_WIDTH);
         gl::UseProgram(program);
@@ -224,6 +227,11 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::Window { win_event: WindowEvent::Resized(w, h), .. } => {
+                    unsafe {
+                        gl::Viewport(0, 0, w, h);
+                    }
+                },
                 Event::KeyDown {
                     keycode: Some(Keycode::J),
                     ..
@@ -268,20 +276,16 @@ fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 
-        const X_STEP: f32 = 2.0 * (1.0 - MARGIN_VW) / (HIGH_BIN - LOW_BIN) as f32;
-        let mut x = X_STEP + MARGIN_VW - 1.0;
         let mut sign = 1.0;
 
+        plot[0] = 0.0;
         for (i, bin) in frequency_bins[LOW_BIN..HIGH_BIN].iter().enumerate() {
             let y = SMOOTHING_TIME_CONST * smoothed_fft[i]
                 + (1.0 - SMOOTHING_TIME_CONST) * gain_mult * bin.abs() * NORMALIZATION;
 
             smoothed_fft[i] = y;
+            plot[i + 1] = sign * y;
 
-            plot[2 * i] = x;
-            plot[2 * i + 1] = sign * y;
-
-            x += X_STEP;
             sign *= -1.0;
         }
 
